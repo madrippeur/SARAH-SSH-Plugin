0789f6e1ab4d613bcefb4d8696618616eb8ebe6e
Wifsimster <wifsimster@gmail.com>
2013-06-24 23:55:17 +0200

    First commit

---
+++ ssh/bin/scripts/cd.sh
@@ -0,0 +1,3 @@
+#exemple pour ejecter le lecteur cd du serveur ssh
+
+eject /dev/sr0
---
+++ ssh/ssh.js
@@ -0,0 +1,22 @@
+﻿
+exports.action = function(data, callback, config, SARAH){
+    var api_url;
+    config = config.modules.ssh;
+
+	var exec = require('child_process').exec;
+
+	var ip = config.api_url_ssh_ip;
+	var port = config.api_url_ssh_port;
+	var user = config.api_url_ssh_user;
+	var pass = config.api_url_ssh_pass;
+	var command = data.cmd_bash;
+
+	var process = '%CD%/plugins/ssh/bin/putty';
+
+	process += ' -ssh ' + user + '@' + ip + ' -P ' + port + ' -pw ' + pass + ' -m %CD%/plugins/ssh/bin/scripts/' + command + ' & exit &';
+	console.log(process);
+	var child = exec(process,function (error, stdout, stderr) {""});
+
+	
+	callback({'tts': "tout de suite"}); 
+}
